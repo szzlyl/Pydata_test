dc2003560fb95b87831b4d972c77ae5fd9aa56da
Markliu <szzlyl2@gmail.com>
2018-03-11 15:36:40 +0800

    Define the function
    Remove the wrong definiation

--- Data Translate.py
+++ Data Translate.py
@@ -2,21 +2,31 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-def AddSeries(Input_Series,ind_a)
+def AddSeries(Input_Series,ind_a):
 
 # Add ind_a columns of Input_Series for
 # The array
 # Return the data frame
-
-    Size_In = Input_Series.size
-    T_S = Input_Series
+    size_in = Input_Series.size
+    t_s = Input_Series
     t_i =1
-    For t_i in range(1, ind_a)
-        T_B = pd.Series(np.zeros(t_i)).append(Input_Series[0:Size_In-t_i],ignore_index=True)
-        DF = [T_S,T_B]
-        T_S = pd.concat(DF,axis=1)
+    for t_i in range(1, ind_a)
+        t_b = pd.Series(np.zeros(t_i)).append(Input_Series[0:size_in-t_i],ignore_index=True)
+        DF = [t_s,t_b]
+        t_s = pd.concat(DF,axis=1)
+    return t_s
 
-Return T_S
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    # add one more layer and return the output of this layer
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
 
 #Retrieve the data from Yahoo
 #https://finance.yahoo.com/quote/%5EHSI/history?p=%5EHSI
@@ -40,4 +50,32 @@ data_Close_V= AddSeries(data_M['Close_y'],4)
 Re_D=[data_Close_H,data_Close_V]
 Data_x = pd.concat(Re_D,axis=1)
 
+#split the data with Target and Training data
 
+Data_y = Data_x['Close_x'][5:2000]
+Data_xin = Data_x.iloc[5:2000,1:]
+
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+
+l1 = add_layer(xs, 9, 90, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 90, 1, activation_function=None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                     reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+if int((tf.__version__).split('.')[1]) < 12:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    # training
+    sess.run(train_step, feed_dict={xs: Data_xin, ys: Data_y})
+    if i % 50 == 0:
+        # to see the step improvement
+        print(sess.run(loss, feed_dict={xs: Data_xin, ys: Data_y}))
